3787b8bf19ab4646e38c1088f4a14264cc4d68bf
Lucas <lukmorato@gmail.com>
2024-11-18 19:22:47 +0100

    Pc

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Login from './sections/login/Login';
 import Main from './sections/main/Main';
 
-
-
+// Punto de entrada donde se renderiza la app
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <Router basename='/'>
-        <Routes>
-          <Route exact path ='/login'element={
-            <Login/>
-          }>
-          </Route>
-            <Route path='/' element={<Main/>}/>
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        {/* Definir rutas para cada componente */}
+        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Main />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
-
-
