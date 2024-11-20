2bec23dbd89c5f4f3d33c21355d30b417dc25ff6
lukmorato <lukmorato@gmail.com>
2024-11-20 07:31:46 +0000

    railway

--- frontend/src/assets/constanst/constants.js
+++ frontend/src/assets/constanst/constants.js
@@ -1,5 +1,5 @@
-  // const baseURL = 'https://amused-respect-production.up.railway.app'
-  const baseURL = 'http://localhost:3500'
+  const baseURL = 'https://amused-respect-production.up.railway.app'
+  // const baseURL = 'http://localhost:3500'
 
   export {
     baseURL
