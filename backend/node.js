c462bee0d571c577c54e6aad0b469dcf78141068
lukmorato <lukmorato@gmail.com>
2024-11-21 06:59:23 +0000

    cors

--- backend/node.js
+++ backend/node.js
@@ -3,33 +3,21 @@ import cors from 'cors'
 import { createClient } from "@libsql/client";
 import dotenv from 'dotenv';
 const app = express()
-app.use(cors({
-  origin: "*"
-}));
 
-app.options("*", cors());
-
-app.set("trust proxy", true);
-
+app.use(express.json())
+app.use(express.static('public'))
 
 dotenv.config()
 
 let userLogIn = false
-
-
 
 const db = createClient({
   url: process.env.DB_URL,
   authToken: process.env.DB_TOKEN
 })
 
-
-app.use(express.json())
-app.use(express.static('public'))
-
 const port = process.env.PORT
 // const port = 3500
-
 
 await db.execute(`
     CREATE TABLE IF NOT EXISTS userData(
